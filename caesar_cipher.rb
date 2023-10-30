7e456b7705e940f0339bd8bccab90fcc8d9eba45
Noah <100672306+eacxyz@users.noreply.github.com>
2023-10-30 12:59:34 -0400

    Complete caesar cipher

---
+++ caesar_cipher.rb
@@ -0,0 +1,24 @@
+# a-z: 97-122
+# A-Z: 65-90
+
+def caesar_cipher(string, shift)
+	array = string.split("")
+	newArr = array.map do |c|
+		num = c.ord + shift
+		if c.ord < 65 || c.ord > 122 || (c.ord > 90 && c.ord < 97)
+			c
+		elsif num > 122
+			num = num - 122 + 96
+			num.chr
+		elsif num > 90 && c.ord <= 90
+			num = num - 90 + 64
+			num.chr
+		else
+			num.chr
+		end
+	end
+	newArr.join("")
+end
+
+puts caesar_cipher("What a string!", 5)
+puts caesar_cipher("What a string!", 26)
